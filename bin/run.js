3287011c73fa49677dff715362fe0dec7ad31398
Andrew Quartey <hello@yestrup.com>
2018-06-18 15:05:42 +0000

    Moved token to .env

--- bin/run.js
+++ bin/run.js
@@ -1,12 +1,13 @@
 'use strict';
-
+require('dotenv').config();
 const slackClient = require('../server/slackClient');
 const service = require('../server/service');
 const http = require('http');
 console.log(slackClient);
 const server = http.createServer(service);
 
-const token = 'xoxb-383652017927-383657947447-ID1vH7ZdXQ53NGXw5LOhc68n';
+const token = process.env.SLACK_TOKEN;
+console.log(token)
 const rtm = slackClient.init(token);
 rtm.start();
 
